2c42380735ed69760963b051b2c9489387b0fb0c
Mogaka1994 <isaacbill247@gmail.com>
2023-08-22 23:34:57 +0300

    newsapi app

---
+++ app/src/main/java/com/example/newsapi/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.newsapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
